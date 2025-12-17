f9458afe735457e86527b1683fff31fe0f372d48
AbdulRehman4it <abdulrehman226721skp@gmail.com>
2025-12-18 01:14:56 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/**/*.{html,php,css,js}'], // Scans all subfolders within 'build'
+  theme: {
+    extend: {
+     fontFamily: {
+        Satoshi: ['Satoshi', 'sans-serif'],
+         CabinetGrotesk: ['CabinetGrotesk', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
